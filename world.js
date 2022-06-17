19a14fa587ba85038a5c0d59fb3516683a7472fb
Yogesh singh <101388803+Chadharyyogi@users.noreply.github.com>
2022-06-17 14:02:34 +0530

    Add files via upload

---
+++ world.js
@@ -0,0 +1,79 @@
+let search=async ()=>{
+    try{
+    
+        const url=`https:masai-mock-api.herokuapp.com/news/top-headlines?country=us`
+        let res=await fetch(url);
+        let data=await res.json();
+        console.log(data.articles);
+        append(data.articles);
+      
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+search();
+
+let srch=async ()=>{
+    try{
+    
+        const url=`https:masai-mock-api.herokuapp.com/news/top-headlines?country=uk`
+        let res=await fetch(url);
+        let data=await res.json();
+        console.log(data.articles);
+        app(data.articles);
+      
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+srch();
+
+
+
+function append(data){
+    let news_result=document.getElementById("news_result");
+    data.forEach((el)=>{
+        
+        let image=document.createElement("img");
+        image.src=el.urlToImage;
+        
+        let title=document.createElement("h3")
+        title.innerText=el.title;
+
+        // let src=document.createElement("h2");
+        // src.innerText=el.url;
+      
+
+        let div=document.createElement("div");
+        div.append(title,image);
+
+        news_result.append(div);
+        
+    });
+}
+
+
+
+function app(data){
+    
+    data.forEach((el)=>{
+        
+        let image=document.createElement("img");
+        image.src=el.urlToImage;
+      
+        let title=document.createElement("h3")
+        title.innerText=el.title;
+
+        // let src=document.createElement("h2");
+        // src.innerText=el.url;
+      
+        
+        let div=document.createElement("div");
+        div.append(title,image);
+
+        side_news.append(div);
+        
+    });
+}
